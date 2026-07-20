42d2964e1b6842eef28928a9fa81581e57d22e5b
Kaan Volkan <kaan.volkan@cern.ch>
2026-07-20 05:30:35 -0400

    Update Mandelbrot.cu

--- Mandelbrot.cu
+++ Mandelbrot.cu
@@ -283,7 +283,7 @@ void moveZoom(int i, int j, double zoom) {
   //printf("New dx = %e, dy = %e\n", Z.dx, Z.dy);
   printf("New xc = %e, yc = %e\n", Z.xc, Z.yc);
 
-  Z.N = Z.N *1.2;
+  Z.N = Z.N + 50*log(1/Z.w);
   printf("New zoom number is %f\n", Z.w);
   printf("New iteration number is %d\n", Z.N);
   
